78e0edbd5296e9bcd8aff49f64eecb58e2984e1c
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-01-04 01:56:47 +0100

    Apply Fuel and FireResistant ItemBehaviors

--- src/main/kotlin/xyz/xenondevs/vanillahammers/registry/Items.kt
+++ src/main/kotlin/xyz/xenondevs/vanillahammers/registry/Items.kt
@@ -2,25 +2,28 @@ package xyz.xenondevs.vanillahammers.registry
 
 import xyz.xenondevs.nova.item.behavior.Damageable
 import xyz.xenondevs.nova.item.behavior.Enchantable
+import xyz.xenondevs.nova.item.behavior.FireResistant
+import xyz.xenondevs.nova.item.behavior.Fuel
 import xyz.xenondevs.nova.item.behavior.Tool
+import xyz.xenondevs.nova.material.NovaMaterialRegistry.item
 import xyz.xenondevs.nova.material.NovaMaterialRegistry.registerItem
 import xyz.xenondevs.vanillahammers.VanillaHammers
 import xyz.xenondevs.vanillahammers.item.Hammer
 
 object Items {
     
-    val WOODEN_HAMMER = registerItem(VanillaHammers, "wooden_hammer", Tool, Hammer, Damageable, Enchantable)
+    val WOODEN_HAMMER = registerItem(VanillaHammers, "wooden_hammer", Tool, Hammer, Damageable, Enchantable, Fuel)
     val STONE_HAMMER = registerItem(VanillaHammers, "stone_hammer", Tool, Hammer, Damageable, Enchantable)
     val IRON_HAMMER = registerItem(VanillaHammers, "iron_hammer", Tool, Hammer, Damageable, Enchantable)
     val GOLDEN_HAMMER = registerItem(VanillaHammers, "golden_hammer", Tool, Hammer, Damageable, Enchantable)
     val DIAMOND_HAMMER = registerItem(VanillaHammers, "diamond_hammer", Tool, Hammer, Damageable, Enchantable)
-    val NETHERITE_HAMMER = registerItem(VanillaHammers, "netherite_hammer", Tool, Hammer, Damageable, Enchantable)
+    val NETHERITE_HAMMER = registerItem(VanillaHammers, "netherite_hammer", Tool, Hammer, Damageable, Enchantable, FireResistant)
     val EMERALD_HAMMER = registerItem(VanillaHammers, "emerald_hammer", Tool, Hammer, Damageable, Enchantable)
     val LAPIS_HAMMER = registerItem(VanillaHammers, "lapis_hammer", Tool, Hammer, Damageable, Enchantable)
     val QUARTZ_HAMMER = registerItem(VanillaHammers, "quartz_hammer", Tool, Hammer, Damageable, Enchantable)
     val OBSIDIAN_HAMMER = registerItem(VanillaHammers, "obsidian_hammer", Tool, Hammer, Damageable, Enchantable)
     val PRISMARINE_HAMMER = registerItem(VanillaHammers, "prismarine_hammer", Tool, Hammer, Damageable, Enchantable)
-    val FIERY_HAMMER = registerItem(VanillaHammers, "fiery_hammer", Tool, Hammer, Damageable, Enchantable)
+    val FIERY_HAMMER = registerItem(VanillaHammers, "fiery_hammer", Tool, Hammer, Damageable, Enchantable, FireResistant)
     val SLIME_HAMMER = registerItem(VanillaHammers, "slime_hammer", Tool, Hammer, Damageable, Enchantable)
     val ENDER_HAMMER = registerItem(VanillaHammers, "ender_hammer", Tool, Hammer, Damageable, Enchantable)
     
